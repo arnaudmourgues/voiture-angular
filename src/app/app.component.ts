40db65b8742a77f7d94c80b1ccb9b1f7b380ac86
FR10008498 <a.mourgues@quadient.com>
2022-10-27 10:34:29 +0200

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import {Component, OnInit, ElementRef} from '@angular/core';
+import {VoitureService} from "./service/voiture.service";
+import {Voiture} from "./interface/voiture";
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'voiture-angular';
+export class AppComponent implements OnInit{
+  snapForm!: FormGroup;
+  voitures : Voiture[];
+  title = 'voiture-app';
+
+  ngOnInit(): void {
+    this.onGetVoitures();
+    this.snapForm = this.formBuilder.group({
+      color: [null, Validators.required],
+      wheels: [null, Validators.required],
+      location: [null, Validators.required ],
+    });
+  }
+
+  constructor(
+    private voitureService: VoitureService,
+    private formBuilder: FormBuilder
+  )
+  {}
+
+
+  onGetVoitures(): void {
+    this.voitureService.getVoitures().subscribe(
+      (response) => {this.voitures = response}
+    )
+  }
+
+
+  formSubmit(): void {
+    this.voitureService.createVoiture(this.snapForm);
+    console.log(this.snapForm.value)
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,10 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import {
+  applyPolyfills,
+  defineCustomElements
+} from "@what3words/javascript-components/loader";
 
 if (environment.production) {
   enableProdMode();
@@ -10,3 +14,4 @@ if (environment.production) {
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
+defineCustomElements(window).then(applyPolyfills);
